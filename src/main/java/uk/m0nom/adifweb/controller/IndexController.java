c1ae58ad73b43e4a985ff716129581e6962b6db0
Mark Wickens <msw@wickensonline.co.uk>
2023-12-30 21:00:11 +0100

    * new release notes

--- src/main/java/uk/m0nom/adifweb/controller/IndexController.java
+++ src/main/java/uk/m0nom/adifweb/controller/IndexController.java
@@ -1,11 +1,17 @@
 package uk.m0nom.adifweb.controller;
 
+import com.amazonaws.util.IOUtils;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.Objects;
 
 @Controller
 public class IndexController {
@@ -16,12 +22,35 @@ public class IndexController {
     @Value("${build.version}")
     private String pomVersion;
 
+    private String releaseNotes;
 
+    public static String getResource(String classpathLocation) {
+        try {
+            return IOUtils.toString(Objects.requireNonNull(IndexController.class.getResourceAsStream(classpathLocation))
+            );
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Could not read file [ " + classpathLocation + " ] from classpath", e);
+        }
+    }
+    public IndexController() {
+        // Convert Markdown release notes to HTML
+        String releaseNotesMd = getResource("/release-notes.md");
+        MutableDataSet options = new MutableDataSet();
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        // You can re-use parser and renderer instances
+        Node document = parser.parse(releaseNotesMd);
+        releaseNotes = renderer.render(document);
+    }
     @GetMapping("/")
-    public String displayCoordForm(Model model) {
+    public String displayIndexPage(Model model) {
         model.addAttribute("build_timestamp", buildTimestamp);
         model.addAttribute("pom_version", pomVersion);
 
+        //
+        model.addAttribute("release_notes", releaseNotes);
         return "index";
     }
 }
---
+++ src/main/resources/release-notes.md
@@ -0,0 +1,6 @@
+**Version 1.2.6, released 30-DEC-2023**
+ - add Bunkers on the Air as a supported activity
+ - improve resilience when reading ADIF files with empty fields
+ - fix issue where no time is specified for a QSO
+ - fix form submit button in Firefox
+ - upgrade to Java 17
