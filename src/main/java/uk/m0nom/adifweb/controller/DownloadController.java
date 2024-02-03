c3c750d11bf4ba60aca65ec9aaadb6f261fc750e
Mark Wickens <msw@wickensonline.co.uk>
2024-02-03 16:57:49 +0000

    1.3.0  - Move to latest Spring 3.x version  - Handle legacy LAT/LONG in comment correctly for older input ADIF files

--- src/main/java/uk/m0nom/adifweb/controller/DownloadController.java
+++ src/main/java/uk/m0nom/adifweb/controller/DownloadController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.logging.Logger;
 
